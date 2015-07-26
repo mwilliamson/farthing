aa647aafef1f98c30d7ff22fb8aea37775c73a48
Michael Williamson <mike@zwobble.org>
2015-07-26 14:35:16 +0100

    Create Class namedtuple

--- farthing/types.py
+++ farthing/types.py
@@ -2,11 +2,12 @@ import collections
 
 
 def describe(type_):
-    return type_.__module__, type_.__name__
+    return Class(type_.__module__, type_.__name__)
 
 
 def union(values):
     return Union(set(values))
 
 
+Class = collections.namedtuple("Class", ["module", "name"])
 Union = collections.namedtuple("Union", ["values"])
