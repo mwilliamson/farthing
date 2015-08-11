552d80b97745e5c513d19a4fa7967ace9d81b001
Michael Williamson <mike@zwobble.org>
2015-08-11 22:53:59 +0100

    Add basic tests for unions

---
+++ tests/types_tests.py
@@ -0,0 +1,16 @@
+from nose.tools import istest, assert_equal
+
+from farthing import types
+
+
+@istest
+def union_of_one_type_is_that_type():
+    assert_equal(types.describe(int), types.union([types.describe(int)]))
+
+
+@istest
+def unions_are_order_agnostic():
+    assert_equal(
+        types.union([types.describe(str), types.describe(int)]),
+        types.union([types.describe(int), types.describe(str)])
+    )
